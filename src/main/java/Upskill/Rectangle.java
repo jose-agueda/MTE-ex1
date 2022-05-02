b4f267156caf58ec4ba144d36111493a6bd60f3b
Marco Castro <morte.marco@hotmail.com>
2022-05-02 14:24:47 +0100

    cretae rectangle

---
+++ src/main/java/Upskill/Rectangle.java
@@ -0,0 +1,14 @@
+package Upskill;
+
+public class Rectangle {
+
+    private double comprimento;
+    private double largura;
+
+    public Rectangle(double comprimento, double largura) {
+        this.comprimento = comprimento;
+        this.largura = largura;
+    }
+
+
+}
